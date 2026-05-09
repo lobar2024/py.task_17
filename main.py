3e215165f16c74a38eca93b1f617589beb85a795
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-05-09 11:55:16 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def factorial_recursive(n):
+    if n == 0:
+        return 1
+    return n * factorial_recursive(n - 1)
+
+def factorial_iterative(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+print(factorial_recursive(5))  # 120
+print(factorial_iterative(5))  # 120
